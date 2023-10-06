8379236098a7daea54ec0966e2379200e118c87f
Marc Fehling <mafehling.git@gmail.com>
2023-10-06 15:27:42 -0600

    Added eigenvalue estimation.

--- mg-ev-estimator.cc
+++ mg-ev-estimator.cc
@@ -14,6 +14,8 @@
 // ---------------------------------------------------------------------
 
 #include <deal.II/base/geometric_utilities.h>
+#include <deal.II/base/mg_level_object.h>
+#include <deal.II/base/signaling_nan.h>
 
 #include <deal.II/dofs/dof_handler.h>
 
@@ -26,6 +28,11 @@
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/tria.h>
 #include <deal.II/grid/filtered_iterator.h>
+
+#include <deal.II/lac/diagonal_matrix.h>
+#include <deal.II/lac/precondition.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/vector.h>
 
 #include <fstream>
 
@@ -53,7 +60,7 @@ public:
 
 
 
-template<int dim, int spacedim = dim>
+template <int dim, int spacedim = dim>
 class Problem
 {
 public:
@@ -63,13 +70,24 @@ public:
 
 private:
   void setup_scenario();
-
   void output_scenario();
 
   Triangulation<dim, spacedim> triangulation;
 
   hp::FECollection<dim>     fe_collection;
   DoFHandler<dim, spacedim> dof_handler;
+
+
+  void setup_multigrid_matrices();
+  void estimate_eigenvalues();
+  void output_eigenvalues();
+
+  using VectorType                 = Vector<double>;
+  using LevelMatrixType            = SparseMatrix<double>;
+  using SmootherPreconditionerType = PreconditionJacobi<LevelMatrixType>;
+  // MatrixFree: using SmootherPreconditionerType = DiagonalMatrix<VectorType>;
+
+  MGLevelObject<std::unique_ptr<LevelMatrixType>> mg_matrices;
 };
 
 
@@ -164,16 +182,80 @@ Problem<dim, spacedim>::output_scenario()
 
 template<int dim, int spacedim>
 void
+Problem<dim, spacedim>::setup_multigrid_matrices()
+{
+  Assert(false, ExcNotImplemented());
+
+  // setup hierarchy of "cheap" poisson matrices
+  // with corresponding constraint objects including Dirichlet boundary values
+}
+
+
+
+template<int dim, int spacedim>
+void
+Problem<dim, spacedim>::estimate_eigenvalues()
+{
+  using SmootherType = PreconditionChebyshev<LevelMatrixType, VectorType, SmootherPreconditionerType>;
+
+  // Initialize smoothers.
+  const unsigned int min_level = mg_matrices.min_level();
+  const unsigned int max_level = mg_matrices.max_level();
+
+  MGLevelObject<typename SmootherType::AdditionalData> smoother_data(min_level, max_level);
+  for (unsigned int level = min_level; level <= max_level; level++)
+    {
+      smoother_data[level].preconditioner = std::make_shared<SmootherPreconditionerType>();
+
+      // MatrixFree: mg_matrices[level]->compute_inverse_diagonal(smoother_data[level].preconditioner->get_vector());
+      smoother_data[level].preconditioner->initialize(*mg_matrices[level]);
+
+      smoother_data[level].smoothing_range     = 20.;
+      smoother_data[level].degree              = 5;
+      smoother_data[level].eig_cg_n_iterations = 20;
+    }
+
+  // Estimate eigenvalues on all levels, i.e., all operators.
+  std::vector<double> min_eigenvalues(max_level + 1, numbers::signaling_nan<double>());
+  std::vector<double> max_eigenvalues(max_level + 1, numbers::signaling_nan<double>());
+  for (unsigned int level = min_level + 1; level <= max_level; level++)
+    {
+      SmootherType chebyshev;
+      chebyshev.initialize(*mg_matrices[level], smoother_data[level]);
+
+      VectorType vec;
+
+      // MatrixFree: mg_matrices[level]->initialize_dof_vector(vec);
+      vec.reinit(dof_handler.n_dofs());
+
+      const auto evs = chebyshev.estimate_eigenvalues(vec);
+
+      min_eigenvalues[level] = evs.min_eigenvalue_estimate;
+      max_eigenvalues[level] = evs.max_eigenvalue_estimate;
+    }
+}
+
+
+
+template<int dim, int spacedim>
+void
+Problem<dim, spacedim>::output_eigenvalues()
+{
+  Assert(false, ExcNotImplemented());
+}
+
+
+
+template<int dim, int spacedim>
+void
 Problem<dim, spacedim>::run()
 {
   setup_scenario();
-
-  // operator
-  // mgtransfer
-  // estimate ev
-  // output ev
-
   output_scenario();
+
+  setup_multigrid_matrices();
+  estimate_eigenvalues();
+  output_eigenvalues();
 }
 
 
